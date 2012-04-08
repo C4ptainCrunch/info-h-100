2e608d2558954778d44b19edd6520584569e431e
Nikita Marchant <Nikita.Marchant@gmail.com>
2012-04-08 17:51:39 +0200

    Ajout de cli.plateau.py

---
+++ cli/plateau.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def chargerDico(chemin):
+    fichier = open(chemin)
+    liste = []
+    for mot in fichier:
+        liste.append(mot.rstrip())
+    return tuple(liste)
+
+def chargerLettres(chemin):
+    fichier = open(chemin)
+    liste = []
+    for ligne in fichier:
+        array = ligne.split(' ')
+        array[-1] = array[-1].rstrip()
+        liste.append(array)
+    return liste
+
+def creerSac(lettres):
+    lettres = list(lettres)
+    for piece in lettres:
+        del piece[2]
+        pass
+    return lettres
