e41e7b6510dfdf7665da8c7e113f16176677f28d
Lecomte Marceau <marceaulecomte@gmail.com>
2012-04-29 19:22:50 +0200

    ajout fichier

---
+++ doc/examples/cli/afficherChevalet.ex
@@ -0,0 +1,6 @@
+chevalet =  ['R', 'A', 'D', 'E', 'S', 'A', 'S']
+
+
+>>> afficherChevalet(chevalet)
+
+Chevalet : R  A  D  E  S  A  S  
---
+++ doc/examples/cli/demande.ex
@@ -0,0 +1,10 @@
+phrase =  "Nombre de joueurs ?"
+
+
+>>> reponse = demande(phrase)
+
+Nombre de joueurs ? 2
+
+>>> print reponse
+
+2
