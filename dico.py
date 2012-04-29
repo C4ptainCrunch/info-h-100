f9c3d164a8cafc0255f3517cfb8c0c59ae497641
Nikita Marchant <Nikita.Marchant@gmail.com>
2012-04-29 13:06:33 +0200

    Pré-remplissage du .md + docs dans dico.py

--- dico.py
+++ dico.py
@@ -3,4 +3,14 @@
 
 
 def verifier(mot, dico):
+    """
+        Vérifie qu'un mot est bien dans le dictionnaire
+        
+        Arguments :
+        mot (string) : Mot à vérifier, en majuscules.
+        dico (tuple) : Tuple contenant tous les mots autorisés, en majuscules et sans diacritiques
+        
+        Valeur de retour : (bool)
+        Retourne True si le mot est dans le dictionnaire, False sinon
+        """
     return mot in dico
