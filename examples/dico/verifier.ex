30d5435387435d871b8676a3776fdb1266c98068
Lecomte Marceau <marceaulecomte@gmail.com>
2012-04-29 15:50:28 +0200

    exemples

---
+++ examples/dico/verifier.ex
@@ -0,0 +1,17 @@
+mot = 'BONJOUR'
+dico = ('AA', ... ,'EXTENUAIT', ... ,'ZYTHUMS')
+
+
+>>> print verifier(mot, dico)
+
+True
+
+######################################
+
+mot = 'BONJOURES'
+dico = ('AA', ... ,'EXTENUAIT', ... ,'ZYTHUMS')
+
+
+>>> print verifier(mot, dico)
+
+False
